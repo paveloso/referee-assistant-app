d6584c0e9323fc1647e66bc4f92a4f321415fbca
Pavel <pavel@cloudbs.ru>
2021-10-28 23:34:54 +0300

    RAA-003 creates skeleton for the main screen

--- app/src/main/java/com/postscriptum/refereeassistantapp/MainActivity.kt
+++ app/src/main/java/com/postscriptum/refereeassistantapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.postscriptum.refereeassistantapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar_menu, menu)
+        return true
     }
 }
